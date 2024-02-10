714a5bb411d640a44ad5b455713c4b0ff4e143e0
abdullahCode511 <abdullah.alotaibi12020@gmail.com>
2024-02-10 18:08:01 +0300

    reduced settings route and moved it to setting_functions

--- src/flask_website/app.py
+++ src/flask_website/app.py
@@ -11,6 +11,7 @@ from flask_mail import Mail, Message
 import os
 from werkzeug.security import check_password_hash, generate_password_hash
 import db_classes
+import setting_functions
 # from db_classes import Template
 from certificate_models import CertificateEvent, EventType, CertificateForm,CertificateCustomizations,Template
 import uuid
@@ -369,9 +370,10 @@ def allowed_image_file(filename):
 @app.route('/settings')
 @login_required
 def settings():
-    user = current_user
-    # Pass the user's first and last name to the template
-    return render_template('settings.html', first_name=user.Fname, last_name=user.Lname)
+    return setting_functions.settings()
+#     user = current_user
+#     # Pass the user's first and last name to the template
+#     return render_template('settings.html', first_name=user.Fname, last_name=user.Lname)
 
 
 
@@ -381,27 +383,29 @@ def settings():
 @app.route('/settings/change_name', methods=['GET', 'POST'])
 @login_required
 def change_name():
-    form = db_classes.ChangeNameForm()
-    if form.validate_on_submit():
-        current_user.Fname = form.name.data.split()[0]  # assuming the first name is the first word
-        current_user.Lname = ' '.join(form.name.data.split()[1:])  # rest of the parts are considered as the last name
-        db.session.commit()
-        flash('Your name has been updated.', 'success')
-        return redirect(url_for('settings'))
-    return render_template('change_name.html', form=form)
+    return setting_functions.change_name()
+#     form = db_classes.ChangeNameForm()
+#     if form.validate_on_submit():
+#         current_user.Fname = form.name.data.split()[0]  # assuming the first name is the first word
+#         current_user.Lname = ' '.join(form.name.data.split()[1:])  # rest of the parts are considered as the last name
+#         db.session.commit()
+#         flash('Your name has been updated.', 'success')
+#         return redirect(url_for('settings'))
+#     return render_template('change_name.html', form=form)
 
  
 
 @app.route('/settings/change_email', methods=['GET', 'POST'])
 @login_required
 def change_email():
-    form = db_classes.ChangeEmailForm()
-    if form.validate_on_submit():
-        current_user.email = form.email.data
-        db.session.commit()
-        flash('Your email has been updated.', 'success')
-        return redirect(url_for('settings'))
-    return render_template('change_email.html', form=form)
+    return setting_functions.change_email()
+#     form = db_classes.ChangeEmailForm()
+#     if form.validate_on_submit():
+#         current_user.email = form.email.data
+#         db.session.commit()
+#         flash('Your email has been updated.', 'success')
+#         return redirect(url_for('settings'))
+#     return render_template('change_email.html', form=form)
 
 
 
@@ -409,24 +413,25 @@ def change_email():
 @app.route('/settings/change_password', methods=['GET', 'POST'])
 @login_required
 def change_password():
-    form = db_classes.ChangePasswordForm()
-    message = None  # Initialize the message variable
+    return setting_functions.change_password()
+#     form = db_classes.ChangePasswordForm()
+#     message = None  # Initialize the message variable
 
-    if form.validate_on_submit():
-        if bcrypt.check_password_hash(current_user.password, form.old_password.data):
-            if form.new_password.data == form.confirm_new_password.data:
-                # Set new password with bcrypt and decode it to string
-                hashed_password = bcrypt.generate_password_hash(form.new_password.data).decode('utf-8')
-                current_user.password = hashed_password
-                db.session.commit()
-                message = 'Your password has been updated successfully.'
-            else:
-                message = 'New password and confirmation do not match.'
-        else:
-            message = 'Incorrect old password.'
+#     if form.validate_on_submit():
+#         if bcrypt.check_password_hash(current_user.password, form.old_password.data):
+#             if form.new_password.data == form.confirm_new_password.data:
+#                 # Set new password with bcrypt and decode it to string
+#                 hashed_password = bcrypt.generate_password_hash(form.new_password.data).decode('utf-8')
+#                 current_user.password = hashed_password
+#                 db.session.commit()
+#                 message = 'Your password has been updated successfully.'
+#             else:
+#                 message = 'New password and confirmation do not match.'
+#         else:
+#             message = 'Incorrect old password.'
 
-    # Pass the message to the template. If the message is None, nothing will be displayed.
-    return render_template('change_password.html', form=form, message=message)
+#     # Pass the message to the template. If the message is None, nothing will be displayed.
+#     return render_template('change_password.html', form=form, message=message)
 
 
 @app.route('/get_user_info')
